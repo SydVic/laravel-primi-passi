857f8e82c88504cf72de7cefc92f69e70a742aeb
SydVic <fabrizio.bocca27@gmail.com>
2022-06-23 14:21:13 +0200

    aggiunti elementi navbar con data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,25 @@
-<h1>HOME</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel First Step</title>
+  <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+  
+  <section class="navbar">
+    <ul class="nav-items">
+      <li>
+        <a href="">
+          @foreach ($navbar as $navItem)
+              {{$navItem}}
+          @endforeach
+        </a>
+      </li>
+    </ul>
+  </section>
 
-<p>hello world!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'navbar' => [
+            'home' => 'home',
+            'products' => 'products',
+            'aboutUs' => 'about us',
+            'contacts' => 'contacts',
+        ]
+    ];
+    return view('home', $data);
 });
